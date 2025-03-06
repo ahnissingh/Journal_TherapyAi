f013232f3646ef0de08e65485c16a5a1c6a572f1
Ahnis <ahnisaneja@gmail.com>
2025-03-06 21:29:36 +0530

    Downgraded to spring boot 3.4.2 again

--- src/main/java/com/ahnis/journalai/common/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ahnis/journalai/common/security/jwt/JwtAuthenticationFilter.java
@@ -1,98 +1,3 @@
-//
-//package com.ahnis.journalai.common.security.jwt;
-//
-//import com.ahnis.journalai.common.dto.ErrorDetails;
-//import com.ahnis.journalai.common.security.JwtUtil;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import io.jsonwebtoken.ExpiredJwtException;
-//import io.jsonwebtoken.JwtException;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//import java.time.LocalDateTime;
-//
-//@Component
-//@RequiredArgsConstructor
-//@Slf4j
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    private final JwtUtil jwtUtil;
-//    private final UserDetailsService userDetailsService;
-//    private final ObjectMapper objectMapper;
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException, ExpiredJwtException {
-//        try {
-//            final var authHeader = request.getHeader("Authorization");
-//
-//            // Skip if no Bearer token is present
-//            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-//                filterChain.doFilter(request, response);
-//                return;
-//            }
-//
-//            // Extract JWT and username
-//            final var jwt = authHeader.substring(7);
-//            final var username = jwtUtil.extractUsername(jwt);
-//            log.info("JWT token {}", jwt);
-//            log.info("Jwt username {}", username);
-//            // Authenticate if username is valid and no existing authentication
-//            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-//
-//                // Validate token and set authentication
-//                if (jwtUtil.validateToken(jwt, userDetails)) {
-//                    var authToken = new UsernamePasswordAuthenticationToken(
-//                            userDetails,
-//                            null,
-//                            userDetails.getAuthorities() // Include roles from UserDetails
-//                    );
-//                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                    SecurityContextHolder.getContext().setAuthentication(authToken);
-//                    log.info("User authenticated {} ", username);
-//                }
-//            }
-//
-//            filterChain.doFilter(request, response);
-//        } catch (ExpiredJwtException ex) {
-//            var error = new ErrorDetails(
-//                    LocalDateTime.now(),
-//                    "Token expired",
-//                    ex.getMessage());
-//            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//            objectMapper.writeValue(response.getWriter(), error);
-//            return;
-//        } catch (JwtException | AuthenticationException ex) {
-//
-//            var error = new ErrorDetails(
-//                    LocalDateTime.now(),
-//                    "Invalid token. Recheck your JWT token",
-//                    ex.getMessage()
-//            );
-//            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//            objectMapper.writeValue(response.getWriter(), error);
-//            return;
-//        }
-//
-//    }
-//}
 package com.ahnis.journalai.common.security.jwt;
 
 import com.ahnis.journalai.common.dto.ErrorDetails;
@@ -104,6 +9,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
@@ -124,22 +30,15 @@ import java.time.LocalDateTime;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
     private final ObjectMapper objectMapper;
 
     // Add this to save the security context for asynchronous processing
-    private final RequestAttributeSecurityContextRepository securityContextRepository;
+    private final RequestAttributeSecurityContextRepository securityContextRepository = new RequestAttributeSecurityContextRepository();
 
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService, ObjectMapper objectMapper) {
-        //For autowiring
-        this.jwtUtil = jwtUtil;
-        this.userDetailsService = userDetailsService;
-        this.objectMapper = objectMapper;
-        //Not part of spring context
-        this.securityContextRepository = new RequestAttributeSecurityContextRepository();
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain)
