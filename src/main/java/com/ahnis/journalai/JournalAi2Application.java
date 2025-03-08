c96ebbc2ee217d607ad21072f9c48cfabf8683a7
Ahnis <ahnisaneja@gmail.com>
2025-03-08 21:34:05 +0530

    Removed unused plugins and dependenices from pom.xml

--- src/main/java/com/ahnis/journalai/JournalAi2Application.java
+++ src/main/java/com/ahnis/journalai/JournalAi2Application.java
@@ -15,11 +15,14 @@ import java.util.TimeZone;
  * todo 1) Pagination and streak based system
  * todo 2 ) When finalised properties files add the packages here for better
  * todo 4 ) Consider defining all exception messages in yaml file for uniformity
+ * todo 5) i When finalised project add @ConfigurationPropertiesScan classes or packages
+ *        ii Use Mapper Scan for all map struct interfaces
  */
 @SpringBootApplication(exclude = MilvusVectorStoreAutoConfiguration.class)
 @EnableMongoAuditing
 @ConfigurationPropertiesScan
 @EnableScheduling
+
 public class JournalAi2Application {
     //todo reports crud api
     public static void main(String[] args) {
@@ -28,8 +31,6 @@ public class JournalAi2Application {
         SpringApplication.run(JournalAi2Application.class, args);
 
     }
-
-
 }
 
 
